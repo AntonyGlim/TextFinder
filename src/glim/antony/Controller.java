cfeaebb34cf21de54d2ef48522bf2da29e2d96ef
Antony.Glim <Antony.Glim@gmail.com>
2020-01-23 14:40:06 +0300

    Add button "Browse"

--- src/glim/antony/Controller.java
+++ src/glim/antony/Controller.java
@@ -1,4 +1,21 @@
 package glim.antony;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    Button browse;
+
+    public void browse() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File directory = directoryChooser.showDialog(browse.getScene().getWindow());
+        if (directory == null) System.out.println("Nothing selected");
+        else System.out.println(directory.getAbsolutePath());
+    }
+
 }
