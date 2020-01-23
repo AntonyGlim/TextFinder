9318f82f4d1bab2ab7be7ebc3789a3b03ffd5851
Antony.Glim <Antony.Glim@gmail.com>
2020-01-23 16:58:52 +0300

    Add text field with path to file

--- src/glim/antony/Controller.java
+++ src/glim/antony/Controller.java
@@ -2,6 +2,7 @@ package glim.antony;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
@@ -11,11 +12,16 @@ public class Controller {
     @FXML
     Button browse;
 
+    @FXML
+    TextField path;
+
     public void browse() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         File directory = directoryChooser.showDialog(browse.getScene().getWindow());
-        if (directory == null) System.out.println("Nothing selected");
-        else System.out.println(directory.getAbsolutePath());
+        if (directory != null) {
+            System.out.println(directory.getAbsolutePath());
+            path.setText(directory.getAbsolutePath());
+        }
     }
 
 }
